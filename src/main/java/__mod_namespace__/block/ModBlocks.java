b8dfa890d716a66422212e9989ae02f10cfee05f
Markus Bordihn <Markus@Bordihn.de>
2021-10-27 23:17:46 +0200

    Fixed missing SoundType

--- src/main/java/__mod_namespace__/block/ModBlocks.java
+++ src/main/java/__mod_namespace__/block/ModBlocks.java
@@ -2,6 +2,7 @@ package [[ --packageNamespace-- ]].block;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.OreBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.fmllegacy.RegistryObject;
