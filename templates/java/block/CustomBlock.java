ea03bfd328798607dc7f9d6e322ad30e89a3d9ef
Markus Bordihn <Markus@Bordihn.de>
2021-10-28 22:27:29 +0200

    Add custom block

---
+++ templates/java/block/CustomBlock.java
@@ -0,0 +1,19 @@
++++ block/[[ --BlockClassName--]].java
+@@@ create @@@
+public class [[ --BlockClassName--]] extends Block {
+  public [[ --BlockClassName--]](Properties properties) {
+      super(properties);
+  }
+}
+
++++ block/ModBlocks.java
+@@@ after:@TemplateEntryPoint("Register Blocks") @@@
+  public static final RegistryObject<Block> [[ --BLOCK_NAME-- ]] =
+      BLOCKS.register("[[ --block_name-- ]]", () -> new [[ --BlockClassName--]](BlockBehaviour.Properties.of(Material.STONE)
+          .requiresCorrectToolForDrops().strength(3.0F, 6.0F).sound([[ --SoundType-- ]])));
+
++++ item/ModItems.java
+@@@ after:@TemplateEntryPoint("Register Block Items") @@@
+  public static final RegistryObject<Item> [[ --BLOCK_NAME-- ]] =
+      ITEMS.register("[[ --block_name-- ]]", () -> new BlockItem(ModBlocks.[[ --BLOCK_NAME-- ]].get(),
+          new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
